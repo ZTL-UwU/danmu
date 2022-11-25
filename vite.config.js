5530465d60431fddb5964642c2bfce39c352094a
zhangtianli2006 <zhangtianli2006@163.com>
2022-11-25 17:04:07 +0800

    use relative path
    Signed-off-by: zhangtianli2006 <zhangtianli2006@163.com>

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
             resolvers: [ElementPlusResolver()],
         }),
     ],
+    base: './',
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
